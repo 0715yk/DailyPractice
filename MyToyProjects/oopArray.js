7a96b37d5f7fd1d1aad907691b47f5008125048d
hyk1993 <doublep_yk@naver.com>
2020-08-07 23:09:23 +0900

    oopArray.js update

--- MyToyProjects/oopArray.js
+++ MyToyProjects/oopArray.js
@@ -3,18 +3,35 @@
 // 먼저, Array는 클래스, arr=[]와 같이 Array 클래스를 바탕으로 만든 인스턴스가 있음
 // 그러면 먼저, Array 클래스(function형태)를 설계해보자 with 속성(this키워드), 메서드(prototype키워드)
 
-function Array(...elements) {
-    this.length = `${elements.length}개`;
-    Array.prototype.pop = function () {
-        let erased = elements[parseInt(this.length)-1];
-        this.length = `${parseInt(this.length) - 1}개`;
-        return `${erased}을 제거했습니다` ;
+class MyArray {
+    constructor (...elements) {
+        if (elements.length === 1) {
+            this.length = elements[0];
+            let arr = [];
+            for (let i=0;i<this.length;i++) {
+                arr.push(undefined);
+            }
+            this.value = arr;
+            return arr;
+        } else {
+            this.length = elements.length;
+            this.value = elements;
+            return elements;
+        }
+    }
+    push(input) {
+        this.value[length] = input;
+        console.log(this.value.length);
+        return input;
+    }
+    pop() {
+        let deletedValue = this.value[length-1];
+        this.value.splice(length-1, 1);
+        return deletedValue;
     }
 }
 
 let arr = new Array(1,2,3);
-console.log(arr.length);
-console.log(arr.pop());
-console.log(arr.length);
-console.log(arr.pop());
-console.log(arr.length);
+arr.push(4);
+arr.pop();
+console.log(arr);
