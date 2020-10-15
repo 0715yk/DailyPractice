5b5746fa32acdc9aac89c6a0160317ed8f411b2b
Hong-Yong-ki <yongkiniiii@gmail.com>
2020-10-15 16:08:54 +0900

    update

--- algorithm/insertion_sort.js
+++ algorithm/insertion_sort.js
@@ -1,16 +1,22 @@
-let arr3 = [5,6,3,1,10,2,9,7,8,111,2323,0,24,-1];
+let arr3 = [5, 6, 3, 1, 10, 2, 9, 7, 8, 111, 2323, 0, 24, -1];
 
 function insertion_sort(arr) {
-    for (let i=1; i<arr.length; i++) {
-        let max = arr[i];
-        for (let j=i-1;j>=0;j--) {
-            if(max<arr[j]) {
-                let index = arr.indexOf(max);
-                [arr[index], arr[j]] = [arr[j], arr[index]];
+    for (let idx = 0; idx < arr.length; idx++) {
+        let x = idx;
+        for (let compare = idx - 1; compare >= 0; compare--) {
+            if (arr[idx] >= arr[compare]) {
+            } else {
+                let spare = arr[idx];
+                arr[idx] = arr[compare];
+                arr[compare] = spare;
+                idx = compare;
             }
         }
+        idx = x;
     }
     return arr;
 }
+
+
 
 console.log(insertion_sort(arr3));
