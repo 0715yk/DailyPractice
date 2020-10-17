60b26e3ac8b0a59f9bf7291517f594f0b1911d2c
Hong-Yong-ki <yongkiniiii@gmail.com>
2020-10-17 16:05:22 +0900

    move todolist to another repo

--- todolist/src/containers/appendList.js
+++
@@ -1,8 +0,0 @@
-import { connect } from 'react-redux'
-import ToDoLists from '../components/ToDoLists'
-
-const mapStateToProps = (state) => ({
-    lists: state.appendList.list
-});
-
-export default connect(mapStateToProps)(ToDoLists)
